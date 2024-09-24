f1a09099564ae13b8ac9fd38a4731891ffb7dd22
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-24 06:31:42 +0100

    created the apps needed for the project

---
+++ prescription_upload/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrescriptionUploadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'prescription_upload'
---
+++ results_display/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ResultsDisplayConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'results_display'
