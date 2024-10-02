3f00127db91de64c5c632b381ba1640d061f52e1
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-10-02 03:01:44 +0100

    implemented ocr processing using pytesseract

--- prescription_upload/api_views.py
+++ prescription_upload/api_views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import PrescriptionUploadSerializer
 from .models import Prescription
+from ocr_processing.extract_text import extract_text_from_image
 
 class PrescriptionUploadView(APIView):
     def post(self, request, *args, **kwargs):
@@ -17,11 +18,15 @@ class PrescriptionUploadView(APIView):
         upload_data = cloudinary.uploader.upload(image)
         image_url = upload_data.get('url')
 
+        # Extract text using OCR
+        extracted_text = extract_text_from_image(image_url)
+
         #Create a new prescription instance to save image_path
         prescription = Prescription(image_url=image_url)
 
         # Save the Prescription instance with the updated image_url
         prescription.save()
         return Response({
-            'image_url': prescription.image_url
+            'image_url': prescription.image_url,
+            'extracted_text': extracted_text,
         }, status=status.HTTP_201_CREATED)
